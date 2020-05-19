3144f29fe311f3226c6d8eaa7e9174127f8fee8e
Chernyshov_Nikita <xpom5566@mail.ru>
2020-05-19 09:25:15 +0300

    feat: solved 96ms (5.3%) / 24.1mb (6.6%) not so fast but it is working =)

--- main.py
+++ main.py
@@ -33,40 +33,38 @@ class Solution:
 
     if withNode == True:
       if rightNode != None:
-        rightSum = rightNode['0']['sum']
+        rightSum = rightNode['miss']['sum']
       if leftNode != None:
-        leftSum = leftNode['0']['sum']
+        leftSum = leftNode['miss']['sum']
 
     else:
       if rightNode != None:
-        rightSum = rightNode['1']['sum']
-        if rightNode['0']['sum'] > rightNode['1']['sum']:
-          rightSum = rightNode['0']['sum']
+        rightSum = rightNode['include']['sum']
+        if rightNode['miss']['sum'] > rightSum:
+          rightSum = rightNode['miss']['sum']
 
       if leftNode != None:
-        leftSum = leftNode['1']['sum']
-        if leftNode['0']['sum'] > leftNode['1']['sum']:
-          leftSum = leftNode['0']['sum']
+        leftSum = leftNode['include']['sum']
+        if leftNode['miss']['sum'] > leftSum:
+          leftSum = leftNode['miss']['sum']
 
-    maxValue = max(rightSum, leftSum)
-    print('maxChild at %d = %d [r=%d, l=%d]' %
-          (node.val, maxValue, rightSum, leftSum))
+    maxValue = rightSum + leftSum
+    print('maxChild (%s) at %.2f = %.2f [r=%.2f, l=%.2f]' %
+          (withNode, node.val, maxValue, rightSum, leftSum))
 
     return maxValue
 
-  def fill(self, node: Node, level=0) -> None:
+  def fill(self, node: Node) -> None:
     if node == None:
       return
 
     if node not in self.dist:
       # Init self.dist obj
       self.dist[node] = {
-          '0': {
-              'path': str(level),
+          'miss': {
               'sum': 0
           },
-          '1': {
-              'path': str(level),
+          'include': {
               'sum': 0
           },
       }
@@ -76,21 +74,20 @@ class Solution:
     if self.isLeaf(node):
 
       # Fill self.dist
-      self.dist[node]['1'] = {'path': str(node.val), 'sum': node.val}
-      print('leaf reached', node.val, self.dist[node])
+      self.dist[node]['include'] = {'sum': node.val}
+      print('leaf reached', node.val)
+      print(self.dist[node])
       return
 
     else:
-      self.fill(node.right, level + 1)
-      self.fill(node.left, level + 1)
+      self.fill(node.right)
+      self.fill(node.left)
 
       self.dist[node] = {
-          '0': {
-              'path': '+',
+          'miss': {
               'sum': self.maxChildSum(node, False)
           },
-          '1': {
-              'path': '+',
+          'include': {
               'sum': node.val + self.maxChildSum(node, True)
           }
       }
@@ -98,17 +95,24 @@ class Solution:
       return
 
   def rob(self, root: Node) -> int:
+    if root == None:
+      return 0
+
     self.dist = {}
 
     self.fill(root)
     print(self.dist[root])
 
-    return 0
+    rootItem = self.dist[root]
+
+    return rootItem['miss']['sum'] if rootItem['miss']['sum'] > rootItem[
+        'include']['sum'] else rootItem['include']['sum']
 
 
 my = Solution()
 
-n = Node(3, Node(2, Node(2), Node(3)), Node(3, Node(1)))
+n = Node(3, Node(2, Node(3)), Node(3, Node(1)))
+# n = Node(3, Node(20, Node(0), Node(3)), Node(3, Node(1)))
 ans = my.rob(n)
 print("ans", ans)
 '''
@@ -116,5 +120,8 @@ print("ans", ans)
      /     \
    2(b)    3(d)
   /    \       \
-2(f)   3(a)   1(e)
+0.5(f)   3(a)   1(e)
 '''
+
+# Runtime: 96 ms, faster than 5.36% of Python3 online submissions for House Robber III.
+# Memory Usage: 24.1 MB, less than 6.67% of Python3 online submissions for House Robber III.
